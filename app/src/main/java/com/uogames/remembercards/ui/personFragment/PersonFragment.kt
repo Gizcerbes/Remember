eda3793e703cd085bd04be08fd8954f58203c143
gizcerbes@outlook.com <278152andr3328>
2022-07-04 19:11:04 +0300

    Add firebase

--- app/src/main/java/com/uogames/remembercards/ui/personFragment/PersonFragment.kt
+++ app/src/main/java/com/uogames/remembercards/ui/personFragment/PersonFragment.kt
@@ -1,6 +1,23 @@
 package com.uogames.remembercards.ui.personFragment
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.uogames.remembercards.databinding.FragmentPersonBinding
 import dagger.android.support.DaggerFragment
 
-class PersonFragment: DaggerFragment() {
+class PersonFragment : DaggerFragment() {
+
+	private lateinit var bind: FragmentPersonBinding
+
+	override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+		bind = FragmentPersonBinding.inflate(inflater, container, false)
+		return bind.root
+	}
+
+	override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+		super.onViewCreated(view, savedInstanceState)
+	}
+
 }
